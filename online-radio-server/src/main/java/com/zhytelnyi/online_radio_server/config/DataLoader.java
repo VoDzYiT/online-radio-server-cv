89ad6eb06be07ff61f0fee5613375a89d85d4460
Volodymyr Zhytelnyi <volodymyr.zhytelnyi@gmail.com>
2025-11-15 15:31:17 +0200

    Add additional test data

--- online-radio-server/src/main/java/com/zhytelnyi/online_radio_server/config/DataLoader.java
+++ online-radio-server/src/main/java/com/zhytelnyi/online_radio_server/config/DataLoader.java
@@ -32,6 +32,9 @@ public class DataLoader implements CommandLineRunner {
             Track track2 = new Track("Hotel California", "The eagles", "online-radio-server/src/main/resources/music/hotel_california.mp3", 18);
             Track track3 = new Track("Thunderstruck", "AC/DC", "online-radio-server/src/main/resources/music/thunderstruck.mp3", 19);
 
+            Track track4 = new Track("Fur Elise", "Beethoven", "online-radio-server/src/main/resources/music/fur-elise-and-orchestra-110149.mp3", 21);
+            Track track5 = new Track("Symphony No. 40", "Mozart", "online-radio-server/src/main/resources/music/SymphonyNo40InGMinor.mp3", 19);
+
 
             // Плейлисти
             Playlist rockPlaylist = new Playlist("Classic Rock");
@@ -39,14 +42,24 @@ public class DataLoader implements CommandLineRunner {
             rockPlaylist.addTrack(track2);
             rockPlaylist.addTrack(track3);
 
+            Playlist classicPlaylist = new Playlist("Classic Music");
+            classicPlaylist.addTrack(track4);
+            classicPlaylist.addTrack(track5);
 
 
             Station classicRockStation = new Station();
             classicRockStation.setName("Classic Rock Radio");
             classicRockStation.setPlaylists(List.of(rockPlaylist));
 
+            Station classicMusicStation = new Station();
+            classicMusicStation.setName("Classic Music Radio");
+            classicMusicStation.setPlaylists(List.of(classicPlaylist));
+
 
             stationRepository.save(classicRockStation);
+            stationRepository.save(classicMusicStation);
+
+
 
             System.out.println("Test data successfully uploaded.");
         } else {
